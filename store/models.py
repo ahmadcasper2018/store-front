da6e2455984df3f75cd670076d01093df97ce6da
casper <ahmadsy88@gmail.com>
2022-07-06 20:36:20 +0300

    feat: add model (CartItem)

--- store/models.py
+++ store/models.py
@@ -86,3 +86,10 @@ class OrderItem(models.Model):
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
