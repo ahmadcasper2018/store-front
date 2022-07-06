10ee7bb9291b5e20e62517c3b1a8379daa4ba347
casper <ahmadsy88@gmail.com>
2022-07-06 20:33:01 +0300

    feat: add models (Cart,OrderItem)

--- store/models.py
+++ store/models.py
@@ -36,7 +36,6 @@ class Order(models.Model):
 
 
 class Customer(models.Model):
-
     GOLD = 'G'
     SILVER = 'S'
     BRONZ = 'B'
@@ -76,3 +75,14 @@ class OrderItem(models.Model):
         Order,
         on_delete=models.PROTECT,
     )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT
+    )
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=3)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
